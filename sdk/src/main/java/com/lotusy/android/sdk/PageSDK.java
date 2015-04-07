80f80dc3829896d0290a292ccc23cabe0a17e46c
pengshen001 <peng.shen@indochino.com>
2015-04-07 13:57:43 -0700

    add all function prototype of page apis

--- sdk/src/main/java/com/lotusy/android/sdk/PageSDK.java
+++ sdk/src/main/java/com/lotusy/android/sdk/PageSDK.java
@@ -1,7 +1,64 @@
 package com.lotusy.android.sdk;
 
+import com.lotusy.android.sdk.domain.page.*;
+
 /**
  * Created by Indochino on 2015-04-06.
  */
 public class PageSDK extends LotusySDK {
+    public static void UC001_getBusinessDishes(int businessId, PageBusinessDishesCallback callback) {
+
+    }
+
+    public static void UC001_getDishDetails(int dishId, PageDishDetailsCallback callback) {
+
+    }
+
+    public static void UC002_getBuddiesDishActivities(PageBuddyDishActivitiesCallback callback) {
+
+    }
+
+    public static void UC002_getNearByFoods(double lat, double lng, int radius, PageNearByFoodsCallback callback) {
+
+    }
+
+    public static void UC002_getDishPopularity(int dishId, PageDishPopularityCallback callback) {
+
+    }
+
+    public static void UC003_getDishPreference(int dishId, PageDishPreferenceCallback callback) {
+
+    }
+
+    public static void UC003_getDishInfograph(int dishId, PageDishInfographCallback callback) {
+
+    }
+
+    public static void UC004_getBuddiesActivities(PageBuddyDishActivitiesCallback callback) {
+
+    }
+
+    public static void UC004_getMyProfile(PageMyProfileDetailCallback callback) {
+
+    }
+
+    public static void UC004_getMyProfileBuddies(PageMyProfileBuddiesCallback callback) {
+
+    }
+
+    public static void UC004_getMyBuddies(PageMyBuddiesCallback callback) {
+
+    }
+
+    public static void UC004_getNetworkBuddies(PageMyNetworkBuddiesCallback callback) {
+
+    }
+
+    public static void UC004_getSuggestBuddies(PageMySuggestBuddiesCallback callback) {
+
+    }
+
+    public static void UC005_getUserProfileRanking(PageUserProfileCallback callback) {
+
+    }
 }
